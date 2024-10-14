9c7daa731e08c0388353747cfe909137b69b0741
MohamedMenia <mohamed99menia@gmail.com>
2024-10-14 16:38:35 +0300

    Setup Express

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import restaurantsRouter from "./routes/restaurants.js";
 import culisinesRouter from "./routes/culisines.js";
-import dotenv from 'dotenv';
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-dotenv.config();
 
 app.use("/restaurants", restaurantsRouter);
 app.use("/culisines", culisinesRouter);
-
+app.use(errorHandler);
 
 app
   .listen(PORT, () => {
---
+++ utils/responses.ts
@@ -0,0 +1,20 @@
+import type { Response } from "express";
+
+export function successResponse(
+  res: Response,
+  data: any,
+  message: string = "Success"
+) {
+  res.status(200).json({
+    success: true,
+    message,
+    data,
+  });
+}
+
+export function errorResponse(res: Response, status: number, error: any) {
+  res.status(status).json({
+    success: false,
+    error,
+  });
+}
