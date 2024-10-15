26d8a9a906bf9fd75d11660d4076a0f181e04b56
MohamedMenia <mohamed99menia@gmail.com>
2024-10-15 12:17:07 +0300

    redis setup

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { ResturantSchema, type TResturantSchema } from "../schemas/restaurants.js";
+import { initRedisClient } from "../utils/client.js";
 const router = express.Router();
 
 router.post("/", validate(ResturantSchema) ,async (req, res) => {
   try {
-    const data = req.body as TResturantSchema
+    const data = req.body as TResturantSchema;
     res.send("Restaurants");
   } catch (error: any) {
     res.status(500).json({ error: error.message as string });
